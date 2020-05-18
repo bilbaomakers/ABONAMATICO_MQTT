5df39cdd7c5e0dc5191c50e5b698826c684d1210
Diego Maroto <diego.maroto@rdtengineers.com>
2020-05-18 14:24:24 +0200

    LIB IndicadorLed incluido ciclo

--- src/IndicadorLed.cpp
+++ src/IndicadorLed.cpp
@@ -3,37 +3,56 @@
 #include "Arduino.h"
 
 
-
 IndicadorLed::IndicadorLed(int l_pin, bool t_Invertir){
 
     this->pin=l_pin;
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW ^ Invertir);
     EstadoLed = LED_APAGADO;
-    TiempoON = 200;
-    TiempoOFF = 1000;
+    EstadoLedCiclos = LED_APAGADO;
+    TiempoONPulso = 200;
+    TiempoOFFPulso = 1000;
     NumeroPulsos = 1;
-    CuentaMillis1=millis();
-    CuentaCiclos = 0;
+    TiempoONCiclo = 200;
+    TiempoOFFCiclo = 1000;
+    TiempoCiclo = 5000;
+    NumeroPulsosCiclo = 1;
+    CuentaMillisPulso=millis();
+    CuentaMillisCiclo=millis();
+    CuentaPulsos = 0;
     Invertir = t_Invertir;
 
 }
 
-void IndicadorLed::Pulso(unsigned int l_TiempoON, unsigned int l_TiempoOFF, unsigned int l_NumeroPulsos){
+void IndicadorLed::Pulsos(unsigned int l_TiempoONPulso, unsigned int l_TiempoOFFPulso, unsigned int l_NumeroPulsos){
 
-    this->TiempoON=l_TiempoON;
-    this->TiempoOFF=l_TiempoOFF;
+    this->TiempoONPulso=l_TiempoONPulso;
+    this->TiempoOFFPulso=l_TiempoOFFPulso;
     this->NumeroPulsos=l_NumeroPulsos;
-    CuentaCiclos = 0;
+    CuentaPulsos = 0;
+    digitalWrite(pin, LOW ^ Invertir);
     EstadoLed=LED_PULSO_OFF;
 
 }
 
+void IndicadorLed::Ciclo(unsigned int l_TiempoONCiclo, unsigned int l_TiempoOFFCiclo, unsigned int l_TiempoCiclo, unsigned int l_NumeroPulsos){
+
+
+    TiempoONCiclo = l_TiempoONCiclo;
+    TiempoOFFCiclo = l_TiempoOFFCiclo;
+    TiempoCiclo = l_TiempoCiclo;
+    NumeroPulsosCiclo = l_NumeroPulsos;
+    EstadoLed = LED_CICLO;
+    EstadoLedCiclos = LED_CICLO;
+    CuentaMillisCiclo = millis();
+
+}
 
 void IndicadorLed::Encender(){
 
     digitalWrite(pin, HIGH ^ Invertir);
     EstadoLed = LED_ENCENDIDO;
+    EstadoLedCiclos = LED_ENCENDIDO;
 
 }
 
@@ -41,6 +60,7 @@ void IndicadorLed::Apagar(){
 
     digitalWrite(pin, LOW ^ Invertir);
     EstadoLed = LED_APAGADO;
+    EstadoLedCiclos = LED_APAGADO;
 
 }
 
@@ -52,13 +72,13 @@ void IndicadorLed::RunFast(){
         case LED_PULSO_ON:
 
             //Si llevamos Encendidos mas del tiempo encendido
-            if ((millis() - CuentaMillis1) > TiempoON){
+            if ((millis() - CuentaMillisPulso) > TiempoONPulso){
 
                 // Apagar, actualizar el cuentatiempos y el estado y contar un ciclo hecho
                 digitalWrite(pin, LOW ^ Invertir);
-                CuentaMillis1 = millis();
+                CuentaMillisPulso = millis();
                 EstadoLed = LED_PULSO_OFF;
-                if (NumeroPulsos != 0){ CuentaCiclos++;}
+                if (NumeroPulsos != 0){ CuentaPulsos++;}
              
             }
 
@@ -68,28 +88,60 @@ void IndicadorLed::RunFast(){
         case LED_PULSO_OFF:
 
             // Si llevamos apagados mas del tiempo de apagado
-            if ((millis() - CuentaMillis1) > TiempoOFF){
+            if ((millis() - CuentaMillisPulso) > TiempoOFFPulso){
 
                 // Si quedan pulsos por hacer
-                if (CuentaCiclos < NumeroPulsos || NumeroPulsos == 0){
+                if (CuentaPulsos < NumeroPulsos || NumeroPulsos == 0){
                     
                     
                     digitalWrite(pin, HIGH ^ Invertir);      // ENCENDER
-                    CuentaMillis1 = millis();                // ACTUALIZAR EL TIEMPO DEL CONTADOR
+                    CuentaMillisPulso = millis();                // ACTUALIZAR EL TIEMPO DEL CONTADOR
                     EstadoLed = LED_PULSO_ON;                // PASAR A ESTE ESTADO QUE HARA OTRO CICLO
 
                 }
 
                 else{
 
-                    CuentaMillis1 = millis();       // ACTUALIZAR EL TIEMPO DEL CONTADOR                    
-                    EstadoLed=LED_APAGADO;
+                    CuentaMillisPulso = millis();       // ACTUALIZAR EL TIEMPO DEL CONTADOR                    
+                   
+                    EstadoLed=EstadoLedCiclos;      // Volver el led al estado que estaba antes de los pulsos
+
+                    switch (EstadoLedCiclos){       
+                    
+                        case LED_APAGADO:
+
+                            digitalWrite(pin, LOW ^ Invertir);      // APAGAR
+
+                        break;
+
+                        case LED_ENCENDIDO:
+
+                            digitalWrite(pin, HIGH ^ Invertir);      // ENCENDER
+
+                        break;
+
+                        default:
+                        break;
+
+                    }
 
                 }
                 
              
             }
             
+        break;
+
+        case LED_CICLO:
+
+        if ((millis() - CuentaMillisCiclo) > TiempoCiclo){
+
+            this->Pulsos(TiempoONCiclo, TiempoOFFCiclo, NumeroPulsosCiclo);
+            CuentaMillisCiclo = millis();
+
+        }
+
+
         break;
 
         default:
--- src/IndicadorLed.h
+++ src/IndicadorLed.h
@@ -7,13 +7,20 @@ class IndicadorLed{
 private:
 
     int pin;
-    unsigned int TiempoON;
-    unsigned int TiempoOFF;
+
+    unsigned int TiempoONPulso;
+    unsigned int TiempoOFFPulso;
     unsigned int NumeroPulsos;
 
-    unsigned long CuentaMillis1;
-    unsigned int CuentaCiclos;
+    unsigned int TiempoONCiclo;
+    unsigned int TiempoOFFCiclo;
+    unsigned long TiempoCiclo;
+    unsigned int NumeroPulsosCiclo;
 
+    unsigned long CuentaMillisPulso;
+    unsigned long CuentaMillisCiclo;
+    unsigned int CuentaPulsos;
+    
     bool Invertir;
 
 
@@ -25,13 +32,15 @@ public:
         LED_ENCENDIDO,
         LED_PULSO_ON,
         LED_PULSO_OFF,
+        LED_CICLO,
 
-    }EstadoLed;
+    }EstadoLed, EstadoLedCiclos;
 
     IndicadorLed(int l_pin, bool l_Invertir);
     ~IndicadorLed(){};
 
-    void Pulso(unsigned int l_TiempoON, unsigned int l_TiempoOFF, unsigned int l_NumeroPulsos);
+    void Pulsos(unsigned int l_TiempoONPulso, unsigned int l_TiempoOFFPulso, unsigned int l_NumeroPulsos);
+    void Ciclo(unsigned int l_TiempoONPulso, unsigned int l_TiempoOFFPulso, unsigned int l_TiempoCiclo, unsigned int l_NumeroPulsos);
     void Encender();
     void Apagar();
 
