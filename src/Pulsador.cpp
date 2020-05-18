febf5b857602028b6b7cb8a3c727a3b8dbd25c3d
Diego Maroto <diego.maroto@rdtengineers.com>
2020-05-18 18:44:45 +0200

    Pulsador.h Incluido HOLD

--- src/Pulsador.cpp
+++ src/Pulsador.cpp
@@ -14,14 +14,14 @@ Licencia: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0
 #include "Arduino.h"
 
 
-Pulsador::Pulsador(int pin, int modo, unsigned long DebounceTimeMS, bool invert){
+Pulsador::Pulsador(int pin, int modo, unsigned long DebounceTimeMS, unsigned long HoldTimeMS, bool invert){
    	
     pinswitch = pin;
 	pinMode(pinswitch, modo);
-	estado_debounce = EDB_IDLE;
-	estado_debounce_anterior = EDB_IDLE;
+	Estado_Salida = EDB_IDLE;
 	millis_debounce = 0;
-    debouncetime = DebounceTimeMS;
+	debouncetime = DebounceTimeMS;
+	holdtime = HoldTimeMS;
 	invertir=invert;
 
 }
@@ -29,7 +29,7 @@ Pulsador::Pulsador(int pin, int modo, unsigned long DebounceTimeMS, bool invert)
 
 int Pulsador::LeeEstado (){
 
-    return estado_debounce;
+    return Estado_Salida;
 
 }
 
@@ -41,45 +41,61 @@ void Pulsador::Run(){
 
 	// Si he leido un estado distinto de que se supone que tenia el Switch (el estado del switch es una enum, 0,1,2).... 
 	// Estoy comparando un bool con un int pero teoricamente si es 2 pues tampoco es ni 0 ni 1, sera suficientemente listo?
-	if (l_lecturaswhome != estado_debounce_anterior){
-	
-		//Si no esta en DETECTADO_CAMBIO, pues a detectectado y apuntar el tiempo que detecte el cambio
-		if (estado_debounce_anterior != EDB_DETECTADO_CAMBIO){
+	if (l_lecturaswhome != Estado_Salida && Estado_Salida != EDB_DETECTADO_CAMBIO && Estado_Salida != EDB_HOLD){
 
-			millis_debounce = millis();
-			estado_debounce_anterior = EDB_DETECTADO_CAMBIO;
-
-		}
-			
-		// Y si esta en DETECTADO cambio y ha pasado el tiempo de debounce
-		else if ((millis() - millis_debounce) > debouncetime){
-
-			
-			// Aqui ya no es tan listo, a la hora de asignar no le gustan los tipos asi que
-			switch (l_lecturaswhome){
-
-				case LOW:
-
-					estado_debounce = EDB_IDLE;
-					estado_debounce_anterior = EDB_IDLE;
-
-					break;
-
-				case HIGH:
-
-					estado_debounce = EDB_PULSADO;
-					estado_debounce_anterior = EDB_PULSADO;
-
-					break;
-
-			}
-			
-		}
-
-		// Por aqui podriamos implementar otros tiempos de pulsacion de switch
+		millis_debounce = millis();
+		Estado_Salida = EDB_DETECTADO_CAMBIO;
 
 	}
 
-	// Si leo el mismo estado que el que tenia el boton entonces paso del tema.
+	switch(Estado_Salida){	
+
+		// Si esta en DETECTADO_CAMBIO (o sea Debouncing)
+		case EDB_DETECTADO_CAMBIO:
+
+			// Mirar si ha pasado el
+			if ((millis() - millis_debounce) > debouncetime){
+		
+				// Aqui ya no es tan listo, a la hora de asignar no le gustan los tipos asi que
+				switch (l_lecturaswhome){
+
+					case LOW:
+
+						Estado_Salida = EDB_IDLE;
+						
+						break;
+
+					case HIGH:
+
+						Estado_Salida = EDB_PULSADO;
+						
+						break;
+
+				}
+
+			}
+
+		break;
+
+		case EDB_PULSADO:
+
+			if (l_lecturaswhome == HIGH && (millis() - millis_debounce) > holdtime && holdtime != 0){
+
+				Estado_Salida = EDB_HOLD;
+
+			}
+			
+		break;
+
+		case EDB_HOLD:
+
+			if (l_lecturaswhome == LOW && (millis() - millis_debounce) > holdtime){
+
+				Estado_Salida = EDB_IDLE;
+
+			}
+	
+		}
+
 
 }
